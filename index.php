ea12a41f95076fda0d9d2a4a1e8b69a62045fd71
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-04-19 16:38:47 +0200

    creazioni variabili

--- index.php
+++ index.php
@@ -8,11 +8,35 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
+    <?php
+    if(isset($_GET['password'])){
+        $caratteripassword = $_GET['password'];
+    }
+     
+    $letter='abcdefghijklmnopqrstuvwxyz';
+    $number='1234567890';
+    $special='._/!&';
+    $all= $letter . $number . $special;
+    $arrayAll = str_split($all);
+
+    function generatePassword($arrayAll, $caratteripassword){
+        
+    }
+
+
+
+
+    ?>
+
+
+
+
+
     <main>
             <div>
                 <form action="index.php" method="GET">
-                    <label for="passLang">Lunghezza Password:</label>
-                    <input type="number" id="passLang" name="passLang">
+                    <label for="password">Lunghezza Password:</label>
+                    <input type="number" id="password" name="password">
                     <button>Crea Password</button>
                 </form>
             </div>
