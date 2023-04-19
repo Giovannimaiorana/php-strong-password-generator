62a4274d129b8ee8fda55bd1817e8a9abb46bf27
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-04-19 16:18:19 +0200

    Creazione form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <main>
+            <div>
+                <form action="index.php" method="GET">
+                    <label for="passLang">Lunghezza Password:</label>
+                    <input type="number" id="passLang" name="passLang">
+                    <button>Crea Password</button>
+                </form>
+            </div>
+    </main>
+    
+</body>
+</html>
