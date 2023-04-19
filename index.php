75a1ba91f1280959ec65b67e80712288675993f5
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-04-19 17:13:30 +0200

    creazione funzione per generazione password

--- index.php
+++ index.php
@@ -20,11 +20,18 @@
     $arrayAll = str_split($all);
 
     function generatePassword($arrayAll, $caratteripassword){
-        
+        $password=[];
+
+        for ($i =0; $i< $caratteripassword; $i++){
+          $randomnumber= rand(0, count($arrayAll));
+          foreach($arrayAll as $key=> $caratteri){
+            if ($randomnumber==$key){
+                $password[]=$caratteri;
+            }
+          }
+        }
+         return $password;
     }
-
-
-
 
     ?>
 
